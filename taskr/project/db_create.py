603fefc769d424edb064241fa48aeb922fa8a2c0
Sana Javed <sanajaved7@gmail.com>
2015-08-17 19:23:54 -0400

    Refactored code to use SQLAlchemy

--- taskr/project/db_create.py
+++ taskr/project/db_create.py
@@ -1,17 +1,13 @@
-import sqlite3
-from _config import DATABASE_PATH
+from views import db
+from models import Task
+from datetime import date
 
-with sqlite3.connect(DATABASE_PATH) as connection:
+#create db and the db table
+db.create_all()
 
-    #get cursor object
-    c = connection.cursor()
+#insert data
+db.session.add(Task("Finish this tutorial", date(2015,3,3), 10, 1))
 
-    #create the table
-    c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, due_date TEXT NOT NULL, priority INTEGER NOT NULL, status INTEGER NOT NULL)""")
+db.session.add(Task("Finish Real Python", date(2015, 8, 15), 10, 1))
 
-    #insert dummy data
-    c.execute('INSERT INTO tasks(name, due_date, priority, status)' 'VALUES("Finish this tutorial", "08/04/2015", 10, 1)')
-    c.execute(
-        'INSERT INTO tasks(name, due_date, priority, status)'
-        'VALUES("Finish Real Python", "08/04/2015", 10, 1)'
-        )
+db.session.commit()
