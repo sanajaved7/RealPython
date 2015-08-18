41a91756f7f6b6bd8657c3a70347349370208f6b
Sana Javed <sanajaved7@gmail.com>
2015-08-17 21:41:43 -0400

    Added routes for user registration

--- taskr/project/views.py
+++ taskr/project/views.py
@@ -1,4 +1,4 @@
-from forms import AddTaskForm
+from forms import AddTaskForm, RegisterForm, LoginForm
 
 from functools import wraps
 from flask import Flask, flash, redirect, render_template, request, session, url_for, g
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.config.from_object('_config')
 db = SQLAlchemy(app)
 
-from models import Task
+from models import Task, User
 
 def login_required(test):
     @wraps(test)
@@ -68,7 +68,6 @@ def new_task():
             flash('New entry success!')
     return redirect(url_for('tasks'))
 
-
 #Mark tasks as complete
 @app.route('/complete/<int:task_id>/')
 @login_required
@@ -88,3 +87,21 @@ def delete_entry(task_id):
     db.session.commit()
     flash('The task was deleted!')
     return redirect(url_for('tasks'))
+
+#Register users
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    error = None
+    form = RegisterForm(request.form)
+    if form.validate_on_submit():
+        new_user = User(
+            form.name.data,
+            form.email.data,
+            form.password.data,
+            )
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Thanks for registering!!!! :D Please login.')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form, error=error)
+
