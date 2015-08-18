84de39ca5772aa50df7c108ce952dcda4f319749
Sana Javed <sanajaved7@gmail.com>
2015-08-17 21:42:10 -0400

    Updated forms to allow user registration

--- taskr/project/forms.py
+++ taskr/project/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import Form
-from wtforms import TextField, DateField, IntegerField, SelectField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextField, DateField, IntegerField, SelectField, PasswordField
+from wtforms.validators import DataRequired, Length, EqualTo
 
 class AddTaskForm(Form):
     task_id = IntegerField('Priority')
@@ -8,3 +8,13 @@ class AddTaskForm(Form):
     due_date = DateField('Date Due (mm/dd/yyyy)', validators=[DataRequired()], format='%m/%d/%Y')
     priority = SelectField('Priority', validators=[DataRequired()], choices=[('1', '1'), ('2', '2'), ('3', '3'),('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10') ])
     status = IntegerField('Status')
+
+class RegisterForm(Form):
+    name = StringField('Username', validators=[DataRequired(), Length(min=6, max=25)])
+    email = StringField('Email', validators=[DataRequired(), Length(min=6, max=40)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=40)])
+    confirm = PasswordField('Repeat Password', [DataRequired(), EqualTo('password', message='Passwords must match')])
+
+class LoginForm(Form):
+    name = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
