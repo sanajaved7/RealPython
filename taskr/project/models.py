baa0e020274e66377b2a6ad8d46121bfe936cbad
Sana Javed <sanajaved7@gmail.com>
2015-08-17 22:34:47 -0400

    Updated database relationships

--- taskr/project/models.py
+++ taskr/project/models.py
@@ -7,12 +7,15 @@ class Task(db.Model):
     name = db.Column(db.String, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
     priority = db.Column(db.Integer, nullable=False)
+    posted_date = db.Column(db.Date, default=datetime.datetime.utcnow())
     status = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def __init__(self, name, due_date, priority, status):
         self.name = name
         self.due_date = due_date
         self.priority = priority
+        self.posted_date = posted_date
         self.status = status
 
     def __repr__(self):
@@ -26,6 +29,7 @@ class User(db.Model):
     name = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
+    tasks = db.relationship('Task', backref='poster')
 
     def __init__(self, name=None, email=None, password=None):
         self.name = name
